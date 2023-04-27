f538b8883d79e507e6f91590f5a72313a7e84c2d
Lourencio <36267864+Lourencio@users.noreply.github.com>
2023-04-26 23:02:15 -0300

    Update streamlit_app.py
    Corrigindo body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
-st.title('Breakfast Menu')
-st.title('Omega 3 & Blueberry Oatmeal')
-st.title('Kale, Spinach & Rocket Smoothie')
-st.title('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
