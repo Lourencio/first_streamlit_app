90a39ac52fdbc2c0186a8e21799161ed9f842236
Lourencio <36267864+Lourencio@users.noreply.github.com>
2023-04-26 22:59:08 -0300

    Update streamlit_app.py
    Add body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
-st.title('Omega 3 & Bluberry Oatmeal')
+st.title('Breakfast Menu')
+st.title('Omega 3 & Blueberry Oatmeal')
+st.title('Kale, Spinach & Rocket Smoothie')
+st.title('Hard-Boiled Free-Range Egg')
