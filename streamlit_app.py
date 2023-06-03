89eb585a5562b4a381ff04e04b9078f957843013
Lourencio <36267864+Lourencio@users.noreply.github.com>
2023-06-03 19:01:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,9 @@ st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
 
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
