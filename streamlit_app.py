883b1467bdbc4182519de517e60bd4b2ddd609bd
Lourencio <36267864+Lourencio@users.noreply.github.com>
2023-06-03 21:07:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,19 @@ st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #Display the table on the page
 st.dataframe(my_fruit_list)
 
-#Connect to snowflake
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_cur.execute("select * from fruit_load_list")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+st.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursot() as my_cur
+       my_cur.execute("select * from fruit_load_list")
+       return my_cur.fetchall()
 
-#my_data_row = my_cur.fetchone() apenas uma linha
-my_data_row = my_cur.fetchall() 
+# Add a button to load the fruit
+if st.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  st.dataframe(my_data_row)
+  
 
 st.header("The fruit load list contains")
 st.dataframe(my_data_row)
